0e6138a1ab2ca4f243b6eac29918dc6a0ccbf47b
NSH <gus9583@naver.com>
2015-09-15 20:21:09 +0900

    PIDL 추가

---
+++ Common/Text_common.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+namespace SocialC2S
+{
+	//Message ID that replies to each RMI method. 
+               
+    static const ::Proud::RmiID Rmi_RequestLogon = (::Proud::RmiID)(3000+1);
+
+	// List that has RMI ID.
+	extern ::Proud::RmiID g_RmiIDList[];
+	// RmiID List Count
+	extern int g_RmiIDListCount;
+}
+
+namespace SocialS2C
+{
+	//Message ID that replies to each RMI method. 
+               
+    static const ::Proud::RmiID Rmi_ReplyLogon = (::Proud::RmiID)(4000+1);
+
+	// List that has RMI ID.
+	extern ::Proud::RmiID g_RmiIDList[];
+	// RmiID List Count
+	extern int g_RmiIDListCount;
+}
+
+ 
